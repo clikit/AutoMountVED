1ae22d2a2a45ffce33d33438fb471553eb27d2f9
Ambrosio Blanco <ambrob60@hotmail.com>
2017-04-09 10:28:59 +0000

    Merged PR 1: Added new project with basic functionality
    Added new project with basic functionality (just displays a form), and basic unit test (just checks that the form can be run with any exceptions).

---
+++ AutoMountVED/AutoMountVEDTests/Form1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AutoMountVED.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void Form1TestCanCreateForm()
+        {
+            Thread startFormThread = new Thread(StartTheForm);
+
+            try
+            {
+                startFormThread.Start();
+                while (!startFormThread.IsAlive);
+                Thread.Sleep(5000);
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got " + ex.Message);
+            }
+        }
+
+        private void StartTheForm()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
