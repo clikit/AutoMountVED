1757b6a6ebbc9dd5fa024a4ec46b05d6199368be
Ambrosio Blanco <ambrob60@hotmail.com>
2017-04-09 18:07:49 +0800

    Added new project with basic functionality (just displays a form), and basic unit test (just checks that the form can be run with any exceptions).

---
+++ AutoMountVED/AutoMountVED/Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace AutoMountVED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
